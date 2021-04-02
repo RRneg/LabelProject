a819b5fefd2e992f5942785dd1e6cf3d71338f1a
Oleksandr Minaiev <71498446+RRneg@users.noreply.github.com>
2021-04-02 13:22:49 +0300

    Add files via upload

---
+++ AppRunner.java
@@ -0,0 +1,13 @@
+package main.java.com.minaev.crud;
+
+import main.java.com.minaev.crud.view.LabelView;
+
+public class AppRunner {
+
+
+    public static void main(String[] args) {
+        LabelView labelView = new LabelView();
+        labelView.choiceMenu();
+    }
+}
+
